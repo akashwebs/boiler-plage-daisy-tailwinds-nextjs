396074b5d6df25b0bab236a0f1af3231e3b8b761
akashwebs <aksah.shil227@gmail.com>
2022-12-04 20:40:05 +0600

    update

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+import PostCardSinglePage from "../src/Components/PerCatagoriesSection/PostCardSinglePage";
+import SearchBar from "../src/Components/SearchBar";
+
+export default function Home() {
+  return <>Home page</>;
+}
